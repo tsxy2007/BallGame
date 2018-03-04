50830d9e60ed38de982db4403ef4f88f0a525ad0
DESKTOP-NS692FF\飞舞轻扬 <z.tk@163.com>
2018-03-04 12:38:36 +0800

    提交客户端

---
+++ Assets/Scripts/FllowTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FllowTarget : MonoBehaviour {
+
+    public Transform PlayerTransform;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - PlayerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = PlayerTransform.position + offset;
+	}
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+
+    private Rigidbody PlayerBody;
+    public int force = 5;
+    public Text ScoreText;
+    private int score = 0;
+    public GameObject WinText;
+	// Use this for initialization
+	void Start () {
+        PlayerBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (PlayerBody)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            PlayerBody.AddForce( new Vector3(h, 0, v) * force);
+        } 
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string name = collision.collider.tag; // 获取碰撞到游戏物体的名字
+        if (name.Equals("PickUp"))
+        {
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string name = other.tag; // 获取碰撞到游戏物体的名字
+        if (name.Equals("PickUp"))
+        {
+            Destroy(other.gameObject);
+            score++;
+            ScoreText.text = score.ToString();
+            WinText.SetActive( score == 8 );
+        }
+    }
+}
